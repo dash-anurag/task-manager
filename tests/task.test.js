451420cbecd735430504145340ed7e6fcc81d140
Anurag Dash <anuragjeypore1234@gmail.com>
2021-02-20 02:18:10 +0530

    Testing with task data done

--- tests/task.test.js
+++ tests/task.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../src/app");
 const Task = require("../src/models/task");
-const { userOneId, userOne, setupDatabase } = require("./fixtures/db");
+const { userOne, userTwo, taskOne, setupDatabase } = require("./fixtures/db");
 
 beforeEach(async () => {
   await setupDatabase();
@@ -18,4 +18,28 @@ test("Should create task for user", async () => {
   const task = await Task.findById(response.body._id);
   expect(task).not.toBeNull();
   expect(task.completed).toEqual(false);
+});
+
+test("Should fetch user tasks", async () => {
+  const response = await request(app)
+    .get("/tasks")
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+  expect(response.body.length).toBe(2);
+});
+
+test("Should not delete other users tasks", async () => {
+  const response = await request(app)
+    .delete("/tasks/" + taskOne._id)
+    .set("Authorization", `Bearer ${userTwo.tokens[0].token}`)
+    .send()
+    .expect(404);
+
+  const task = await request(app)
+    .get("/tasks/" + taskOne._id)
+    .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200);
+  expect(task).not.toBeNull();
 });
