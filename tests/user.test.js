17f4f2941da4f453f4aa128e72adfb16763c9cef
Anurag Dash <anuragjeypore1234@gmail.com>
2021-02-19 22:04:37 +0530

    Testing an Express Application: 2

---
+++ tests/user.test.js
@@ -0,0 +1,14 @@
+const request = require("supertest");
+
+const app = require("../src/app");
+
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "Anurag Dash",
+      email: "random@gmail.com",
+      password: "default2000",
+    })
+    .expect(201);
+});
